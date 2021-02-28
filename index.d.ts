c1946c6c85b3f10fd82f9abf8d01c3d3cde7af15
danclay <18021681+danclay@users.noreply.github.com>
2021-02-27 20:29:29 -0700

    Added new options
    - disable delete reaction
    - custom page numbers

--- index.d.ts
+++ index.d.ts
@@ -28,6 +28,10 @@ declare module 'eris-pagination' {
     forthButton?: string;
     /** Which page of the submitted embed array should be shown first. Defaults to: 1 (The 1st page / element in the array) */
     startPage?: number;
+	/** Custom page numbers which includes placeholders (e.g. "Page **{current}** of **{total}**") */
+	pageContent?: string;
+	/** Whether the delete options should exist */
+	disableDelete?: boolean;
   }
 
   /**
